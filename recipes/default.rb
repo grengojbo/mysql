b054b24edffbc25dbc80ac338c983099985c0886
Oleg Dolya <oleg.dolya@gmail.com>
2012-06-24 17:19:53 +0300

    Added mysql type (mysql,percona)

--- recipes/default.rb
+++ recipes/default.rb
@@ -16,5 +16,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-
+if node["mysql"]["type"] == "mysql"
 include_recipe "mysql::client"
+end
